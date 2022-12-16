528a05040a643550c71f235911f1268a5be319c1
jkelly3rd <jkelly3rd@gmail.com>
2022-12-16 11:58:34 -0500

    Update data code for city LA

--- scripts/run_markdowns_lacity.R
+++ scripts/run_markdowns_lacity.R
@@ -8,7 +8,7 @@ library(rmarkdown)
 # Load RDS
 murders_district <- readRDS("scripts/rds/murders_district.rds")
 murders_city <- readRDS("scripts/rds/murders_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker.Rmd', 
                   output_dir = "docs",
@@ -18,7 +18,7 @@ rmarkdown::render('scripts/Los_Angeles_Safety_Tracker.Rmd',
 # Load RDS
 burglaries_district <- readRDS("scripts/rds/burglaries_district.rds")
 burglaries_city <- readRDS("scripts/rds/burglaries_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Burglaries.Rmd', 
                   output_dir = "docs",
@@ -28,7 +28,7 @@ rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Burglaries.Rmd',
 # Load RDS
 thefts_district <- readRDS("scripts/rds/thefts_district.rds")
 thefts_city <- readRDS("scripts/rds/thefts_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Thefts.Rmd', 
                   output_dir = "docs",
@@ -38,7 +38,7 @@ rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Thefts.Rmd',
 # Load RDS
 autothefts_district <- readRDS("scripts/rds/autothefts_district.rds")
 autothefts_city <- readRDS("scripts/rds/autothefts_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_VehicleThefts.Rmd', 
                   output_dir = "docs",
@@ -48,7 +48,7 @@ rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_VehicleThefts.Rmd',
 # Load RDS
 robberies_district <- readRDS("scripts/rds/robberies_district.rds")
 robberies_city <- readRDS("scripts/rds/robberies_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Robberies.Rmd', 
                   output_dir = "docs",
@@ -58,7 +58,7 @@ rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Robberies.Rmd',
 # Load RDS
 assaults_district <- readRDS("scripts/rds/assaults_district.rds")
 assaults_city <- readRDS("scripts/rds/assaults_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Assaults.Rmd', 
                   output_dir = "docs",
@@ -68,7 +68,7 @@ rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_Assaults.Rmd',
 # Load RDS
 sexassaults_district <- readRDS("scripts/rds/sexassaults_district.rds")
 sexassaults_city <- readRDS("scripts/rds/sexassaults_city.rds")
-asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
+lapd_asofdate <- readRDS("scripts/rds/lapd_asofdate.rds")
 # Render page
 rmarkdown::render('scripts/Los_Angeles_Safety_Tracker_SexualAssaults.Rmd', 
                   output_dir = "docs",
